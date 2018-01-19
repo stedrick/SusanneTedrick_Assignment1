496bb4b3614d5e73293d6e7c491030db20855ec0
Susanne Tedrick <stedrick@users.noreply.github.com>
2018-01-18 20:59:17 -0600

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,21 @@
+
+//for loop to constrain numbers from 1 - 100
 for (var x=1; x <= 100; x++)
 
 {
-//to find the least common multiple of 3 and 15
+//to test the number as a least common multiple of both 3 and 5 (15)
     if (x % 15 == 0)
-
         console.log("FizzBuzz");
-//to find the numbers that are multiples of 3 only
+    
+//to test the number as a multiple of 3 only
     else if (x % 3 == 0)
 
         console.log('Fizz');
-//to find the numbers that are multiples of 5 only
+//to test a number as a multiple of 5 only
     else if (x % 5 == 0)
-
         console.log('Buzz');
 
     else
-//if none of the conditions are satisfied, then just print number
+//if the number does not meet previous conditions, then just print number
         console.log(x);
-
 }
